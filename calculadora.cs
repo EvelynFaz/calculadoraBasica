7967d0a8e2d6fdf143b45328b663b819f693193c
evelynFaz <eyy.faz.yavirac.edu.ec>
2018-06-04 20:52:45 -0500

    primer commit

---
+++ calculadora.cs
@@ -0,0 +1,83 @@
+namespace poo
+{
+    public class Calculadora
+
+    {
+       double memoria= 0;
+     
+        public double sumar(double sumando1 ,double sumando2)
+
+        {
+         memoria = sumando1 + sumando2;
+            return memoria;
+        }
+
+          public double sumar(double sumando1)
+
+        {
+         memoria = memoria + sumando1;
+            return memoria;
+        }
+      
+      
+        public double resta(double resta ,double minuendo)
+
+        {
+         memoria = resta + minuendo;
+            return memoria;
+        }
+
+          public double resta(double resta )
+
+        {
+         memoria = memoria + resta ;
+            return memoria;
+        }
+
+public double multiplicacion(double multiplicando1 ,double multiplicando2)
+
+        {
+         memoria = multiplicando1 + multiplicando2;
+            return memoria;
+        }
+
+          public double multiplicando(double multiplicando1)
+
+        {
+         memoria = memoria + multiplicando1;
+            return memoria;
+        }
+
+
+        public double divicion(double divicion1 ,double divicion2)
+
+        {
+         memoria = divicion1 + divicion2;
+            return memoria;
+        }
+
+          public double sumar(double divicion1)
+
+        {
+         memoria = memoria + divicion1;
+            return memoria;
+        }
+
+
+public double raiz(double raiz1 ,double raiz2)
+
+        {
+         memoria = raiz1 + raiz2;
+            return memoria;
+        }
+
+          public double raiz(double raiz1)
+
+        {
+         memoria = memoria + raiz1;
+            return memoria;
+        }
+
+
+    }
+}
